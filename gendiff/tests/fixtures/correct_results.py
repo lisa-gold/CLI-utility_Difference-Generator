06766086660fe20b64796afb0d5b69541712ff95
Elizaveta <ukrop@LAPTOP-T43LJSG0>
2023-10-12 18:45:25 +0300

    corrections

--- gendiff/tests/fixtures/correct_results.py
+++ gendiff/tests/fixtures/correct_results.py
@@ -38,7 +38,7 @@ def test_generate_diff_nested_exp():
         }
         setting6: {
             doge: {
-              - wow:
+              - wow: 1
               + wow: so much
             }
             key: value
@@ -78,7 +78,7 @@ Property 'common.setting2' was removed
 Property 'common.setting3' was updated. From true to null
 Property 'common.setting4' was added with value: 'blah blah'
 Property 'common.setting5' was added with value: [complex value]
-Property 'common.setting6.doge.wow' was updated. From '' to 'so much'
+Property 'common.setting6.doge.wow' was updated. From 1 to 'so much'
 Property 'common.setting6.ops' was added with value: 'vops'
 Property 'group1.baz' was updated. From 'bas' to 'bars'
 Property 'group1.nest' was updated. From [complex value] to 'str'
@@ -91,7 +91,7 @@ def test_json_style_exp():
     return '{"common": {"+ follow": false, "setting1": "Value 1", "\
 - setting2": 200, "- setting3": true, "+ setting3": null, "+ set\
 ting4": "blah blah", "+ setting5": {"key5": "value5"}, "setting6"\
-: {"doge": {"- wow": "", "+ wow": "so much"}, "key": "value", "+ op\
+: {"doge": {"- wow": 1, "+ wow": "so much"}, "key": "value", "+ op\
 s": "vops"}}, "group1": {"- baz": "bas", "+ baz": "bars", "foo": "bar\
 ", "- nest": {"key": "value"}, "+ nest": "str"}, "- group2": {"abc"\
 : 12345, "deep": {"id": 45}}, "+ group3": {"deep": {"id": {"number\
