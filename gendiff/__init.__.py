f218d812c0d53f51cecc080bacdc7eb95fd51ddb
Elizaveta <ukrop@LAPTOP-T43LJSG0>
2023-10-12 15:58:25 +0300

    rename __init__.py

--- gendiff/__init.__.py
+++
@@ -1,6 +0,0 @@
-from gendiff.generate_diff_func import generate_diff
-
-
-__all__ = (
-    'generate_diff',
-)
---
+++ gendiff/__init__.py
@@ -0,0 +1,6 @@
+from gendiff.generate_diff_func import generate_diff as generate_diff
+
+
+__all__ = (
+    'generate_diff',
+)
