f1b8a972d224a8e041f1e455023063c2c9ec97c5
Elizaveta <ukrop@LAPTOP-T43LJSG0>
2023-10-21 14:11:04 +0300

    update pile_opener

--- gendiff/file_opener.py
+++ gendiff/file_opener.py
@@ -9,7 +9,8 @@ def parse_as_dict(content, extension):
     elif extension in ['yml', 'yaml']:
         return yaml.safe_load(content)
     else:
-        raise Exception('Wrong extension! Supported exyensions: json, yml, yaml')
+        raise Exception(
+            'Wrong extension! Supported extensions: json, yml, yaml')
 
 
 def open_file(file_path):
